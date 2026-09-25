ff6fe5cd5a447be96fbf16f1766ccbad4815c466
MultiCodigo c2 <c2@punchi.dev>
2026-09-25 18:03:09 +0000

    tarea 607: rediseño el footer con enlaces a Mi cuenta y Ayuda, linea de marca y estilo responsive
    Agent: c2

--- src/app/components/footer/footer.spec.ts
+++ src/app/components/footer/footer.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { Footer } from './footer';
 
 describe('Footer', () => {
@@ -7,6 +8,7 @@ describe('Footer', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Footer],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Footer);
